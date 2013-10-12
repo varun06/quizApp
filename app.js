6ad4e9e371d4a3eb314bec4f723be1ef1962084a
Varun <varunkuamr.saini06@gmail.com>
2013-10-12 08:18:55 -0500

    Added bio data
    Added bio data

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 (function() {
-    var questions = [{"text": "Images/BillGates.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Bill Gates", "explanation": "Bill Gates"}, {"text": "Images/SteveJobs.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Steve Jobs", "explanation": "Steve Jobs"}, {"text": "Images/LarryPage.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Larry Page", "explanation": "Larry Page"}, {"text": "Images/JeffBezos.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Jeff Bezos", "explanation": "Jeff Bezos"}, {"text": "Images/ElonMusk.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Elon Musk", "explanation": "Elon Musk"}];
+    var questions = [
+    {"text": "Images/BillGates.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Bill Gates", "bio": "William Henry Bill Gates III (born October 28, 1955) is an American business magnate, investor, programmer, inventor and philanthropist. Gates is the former chief executive and current chairman of Microsoft, the world’s largest personal-computer software company.Quote - 'Life is not fair; get used to it.'"},
+    {"text": "Images/SteveJobs.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Steve Jobs", "bio": "Steven Paul Steve Jobs (February 24, 1955 – October 5, 2011) was an American entrepreneur, marketer, and inventor, who was the co-founder, chairman, and CEO of Apple Inc. Through Apple, he is widely recognized as a charismatic pioneer of the personal computer revolution and for his influential career in the computer and consumer electronics fields, transforming one industry after another, from computers and smartphones to music and movies.Quote - 'I want to put a ding in the universe.'"},
+    {"text": "Images/LarryPage.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Larry Page", "bio": "Lawrence Larry Page (born March 26, 1973) is an American computer scientist and Internet entrepreneur who is the co-founder of Google, alongside Sergey Brin. On April 4, 2011, Page succeeded Eric Schmidt as the chief executive officer of Google.Quote - 'If you are changing the world, you are working on important things. You are excited to get up in the morning.'"},
+    {"text": "Images/JeffBezos.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Jeff Bezos", "bio": "Jeffrey Preston Jeff Bezos (born January 12, 1964) is an American Internet entrepreneur and investor. He is a technology entrepreneur who has played a key role in the growth of e-commerce as the founder and CEO of Amazon.com, an online merchant of books and later of a wide variety of products. Under his guidance, Amazon.com became the largest retailer on the World Wide Web and a top model for Internet sales.Quote - 'I believe you have to be willing to be misunderstood if you are going to innovate.'"},
+      {"text": "Images/ElonMusk.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Elon Musk", "bio": "Elon Musk (born in South Africa, 28 June 1971) is an South-African business magnate, investor, and inventor. He is currently the CEO & CTO of SpaceX and CEO & Chief Product Architect of Tesla Motors.Quote - 'Failure is an option here. If things are not failing, you are not innovating enough.'"}];
 
     var counter = 0,
         quizOver = false,
@@ -8,6 +13,7 @@
         actionButton = $('#submit'),
         answersBlock = $('.answers'),
         questionTitle = $('.question img'),
+        showResult = $('#result'),
         errorMessage = $('#error_message'),
         questionNumber = $('#question_number'),
         controlsMessage = $('#controls_message'),
@@ -19,7 +25,7 @@
                 .replace(/\{\{1\}\}/g, text);
     };
 
-    var calculateScore = function(answers) {
+    var calculatePercentage = function(answers) {
             var correctCount = 0;
             answers.map(function(value, index) {
                 if (value) correctCount += 1;
@@ -33,13 +39,16 @@
                 quizOver = false;
                 results = {count: 0, answers: [], currentScore: 0};
                 scoreNode.addClass('no_score').text('');
+                showResult.css('display', 'none');
+                questionTitle.show();
                 nextQuestion();
             }
     };
 
     var showResults = function() {
             answersBlock.empty();
-            questionTitle.text('You answered ' + results.currentScore + '% of '
+            questionTitle.css('display','none');
+            showResult.text('You answered ' + results.currentScore + '% of '
                 + 'the questions correctly.');
             quizOver = true;
             actionButton.text('Play again');
@@ -47,9 +56,9 @@
 
     var inputCheck = function() {
             if ($('.answer_radio:checked').length) {
-                transitionEffect(gradeQuestion);
+                gradeQuestion();
             } else {
-                overlayMessage(errorMessage, {borderRadius: '3px', width: '35%', left: '32%'}, 3000);
+                overlayMessage(errorMessage, {borderRadius: '50%', width: '35%', left: '32%'}, 1000);
             }
     };
 
@@ -62,40 +71,33 @@
             }
     };
 
-    var transitionEffect = function(func) {
-            $('.question').hide('slide', {direction: 'left'}, 200, function() {
-                func();
-                $('.question').show('slide', {direction: 'right'}, 200);
-            });
-    };
-
     var dispatchAction = function() {
             var action = actionButton.text();
             if (action === 'Answer') {
                 inputCheck();
             } else if (action === 'Next question') {
-                transitionEffect(nextQuestion);
+                nextQuestion();
             } else if (action === 'Play again') {
-                transitionEffect(resetQuiz);
+                resetQuiz();
             } else {
-                transitionEffect(showResults);
+                showResults();
             }
     };
 
     var gradeQuestion = function() {
             var chosenAnswer = $('.answer_radio:checked + .answer').text(),
                 correctAnswer = questions[counter-1].answer,
-                explanation = questions[counter-1].explanation,
+                bio = questions[counter-1].bio,
                 correct = chosenAnswer === correctAnswer,
                 correctText = correct ? 'Correct! ' : 'Incorrect! The correct answer was ';
 
             results.answers.push(correct);
-            results.currentScore = calculateScore(results.answers);
+            results.currentScore = calculatePercentage(results.answers);
 
             scoreNode.removeClass('no_score')
                 .text(results.currentScore + '% correct');
             questionTitle.html(correctText + wrap_html('span', correctAnswer)).addClass('show_answer');
-            answersBlock.text(explanation);
+            answersBlock.text(bio);
             if (counter === questions.length) {
                 actionButton.text('See results');
             } else {
