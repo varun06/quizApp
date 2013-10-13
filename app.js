262b774c42fe05f9dc19c3135474b0ae8df0c8c5
Varun <varunkuamr.saini06@gmail.com>
2013-10-13 09:35:38 -0500

    Added comments
    Added comments

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 (function() {
+    //Question object
     var questions = [
     {"text": "Images/BillGates.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Bill Gates", "bio": "William Henry Bill Gates III (born October 28, 1955) is an American business magnate, investor, programmer, inventor and philanthropist. Gates is the former chief executive and current chairman of Microsoft, the world’s largest personal-computer software company.Quote - 'Life is not fair; get used to it.'"},
     {"text": "Images/SteveJobs.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Steve Jobs", "bio": "Steven Paul Steve Jobs (February 24, 1955 – October 5, 2011) was an American entrepreneur, marketer, and inventor, who was the co-founder, chairman, and CEO of Apple Inc. Through Apple, he is widely recognized as a charismatic pioneer of the personal computer revolution and for his influential career in the computer and consumer electronics fields, transforming one industry after another, from computers and smartphones to music and movies.Quote - 'I want to put a ding in the universe.'"},
@@ -6,6 +7,7 @@
     {"text": "Images/JeffBezos.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Jeff Bezos", "bio": "Jeffrey Preston Jeff Bezos (born January 12, 1964) is an American Internet entrepreneur and investor. He is a technology entrepreneur who has played a key role in the growth of e-commerce as the founder and CEO of Amazon.com, an online merchant of books and later of a wide variety of products. Under his guidance, Amazon.com became the largest retailer on the World Wide Web and a top model for Internet sales.Quote - 'I believe you have to be willing to be misunderstood if you are going to innovate.'"},
       {"text": "Images/ElonMusk.jpg", "options": ["Bill Gates", "Steve Jobs", "Elon Musk", "Larry Page", "Jeff Bezos"], "answer": "Elon Musk", "bio": "Elon Musk (born in South Africa, 28 June 1971) is an South-African business magnate, investor, and inventor. He is currently the CEO & CTO of SpaceX and CEO & Chief Product Architect of Tesla Motors.Quote - 'Failure is an option here. If things are not failing, you are not innovating enough.'"}];
 
+    //Variable declaration and assignment
     var counter = 0,
         quizOver = false,
         resetCounter = 0,
@@ -20,11 +22,13 @@
         answerTemplate = $('#answer_template').text(),
         results = {answers: [], currentScore: 0};
 
+    //This function handle the text display
     var wrap_html = function(tag, text) {
             return '<{{0}}>{{1}}</{{0}}>'.replace(/\{\{0\}\}/g, tag)
                 .replace(/\{\{1\}\}/g, text);
     };
 
+    //This function calculate the percentage of current score
     var calculatePercentage = function(answers) {
             var correctCount = 0;
             answers.map(function(value, index) {
@@ -33,6 +37,7 @@
             return Math.round(100 * correctCount / answers.length);
     };
 
+    //This function is for reseting the quiz
     var resetQuiz = function() {
             if (resetCounter++ > 1 || quizOver || confirm('Are you sure you want to reset the quiz?')) {
                 counter = 0;
@@ -44,7 +49,7 @@
                 nextQuestion();
             }
     };
-
+    //This function handle the results
     var showResults = function() {
             answersBlock.empty();
             questionTitle.css('display','none');
@@ -54,24 +59,23 @@
             actionButton.text('Play again');
     };
 
+    //This function check the input value
     var inputCheck = function() {
             if ($('.answer_radio:checked').length) {
                 gradeQuestion();
             } else {
-                overlayMessage(errorMessage, {borderRadius: '50%', width: '35%', left: '32%'}, 1000);
+                showErrorMessage();
             }
     };
 
-    var overlayMessage = function(message, css, timeout) {
-            $.blockUI({ message: message, css: css });
-            if (timeout) {
-                setTimeout(function() {
-                    $.unblockUI();
-                }, timeout);
-            }
+    //Show alert if user doesn't select any option
+    var showErrorMessage = function() {
+        var message = "Please select a value";
+        alert(message);
     };
 
-    var dispatchAction = function() {
+    //This function handles the button action
+    var buttonAction = function() {
             var action = actionButton.text();
             if (action === 'Answer') {
                 inputCheck();
@@ -84,6 +88,7 @@
             }
     };
 
+    // The function grade the question based on user input
     var gradeQuestion = function() {
             var chosenAnswer = $('.answer_radio:checked + .answer').text(),
                 correctAnswer = questions[counter-1].answer,
@@ -104,7 +109,7 @@
                 actionButton.text('Next question');
             }
     };
-
+    //This funciton bring up the next question on page
     var nextQuestion = function() {
             var questionDetails = questions[counter];
             questionNumber.text((counter+1) + ' of ' + questions.length);
@@ -123,6 +128,7 @@
             counter++;
     };
 
+    // Function to create a BLINK TEXT effect
     var blink = function(selector){
       $(selector).animate({opacity:0}, 50, "linear", function(){
         $(this).delay(800);
@@ -134,11 +140,10 @@
     };
 
     $('#reset').click(resetQuiz);
-    $('#submit').click(dispatchAction);
+    $('#submit').click(buttonAction);
 
     // Load first question
     nextQuestion();
-
-    // Function to create a BLINK TEXT effect
+    //Blink
     blink('h1 #blink');
 }());
